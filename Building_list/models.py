a89d444fb8b69d69ab56601e46dedde1216b32a3
jiwoo <hjw6112@konkuk.ac.kr>
2020-08-29 09:12:43 +0900

    final2

--- Building_list/models.py
+++ Building_list/models.py
@@ -7,36 +7,38 @@ FlAG = [
     ('사용중', '사용중'),
     ('빈강의실', '빈강의실'),
 ]
-WEEK = [ #첫번재가 장고, 두번째가 탬플릿
+WEEK = [  # 첫번재가 장고, 두번째가 탬플릿
     ('mon', '월'),
     ('tue', '화'),
     ('wed', '수'),
     ('thu', '목'),
     ('fri', '금'),
 ]
+
+
 class Room(models.Model):
     # 호
     number = models.CharField(max_length=10, default='')
     # 사용여부
     on_off = models.CharField(max_length=50, choices=FlAG, default='빈강의실')
-    #요일
+    # 요일
     day_of_the_week = models.CharField(max_length=20, choices=WEEK, default='월')
-    #사용 가능 시간
+    # 사용 가능 시간
     available_time = models.TimeField(default='')
-    #가장 가까운 강의 이름
+    # 가장 가까운 강의 이름
     name = models.TextField(max_length=100, default='')
-    #층
+    # 층
     floor = models.TextField(max_length=10, default='')
-    #시간표
+    # 시간표
     img = models.ImageField(upload_to='timeline_photo/%Y/%m/%d')
 
     def __str__(self):
-        return self.number+"호/ "+self.day_of_the_week+ "요일"
+        return self.number + "호/ " + self.day_of_the_week + "요일"
 
     def created_string(self):
         time_now = timezone.now().time()
 
-        time = datetime.combine(date.min, self.available_time)-datetime.combine(date.min, time_now)
+        time = datetime.combine(date.min, self.available_time) - datetime.combine(date.min, time_now)
         if time < timedelta(minutes=1):
             return '수업 없음'
         elif time < timedelta(hours=1):
@@ -46,14 +48,15 @@ class Room(models.Model):
         else:
             return '수업 없음'
 
+
 class Lecture(models.Model):
     # 룸 모델
     room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name="lecture_room")
     # 강의 명
     name = models.TextField(max_length=100, default='')
-    #수업 시작 시간
+    # 수업 시작 시간
     start_time = models.TimeField(default='')
-    #수업 종료 시간
+    # 수업 종료 시간
     finish_time = models.TimeField(default='')
 
     def __str__(self):
@@ -62,7 +65,7 @@ class Lecture(models.Model):
     def created_string(self):
         time_now = timezone.now().time()
 
-        time = datetime.combine(date.min, self.start_time)-datetime.combine(date.min, time_now)
+        time = datetime.combine(date.min, self.start_time) - datetime.combine(date.min, time_now)
         if time < timedelta(minutes=1):
             return 'false'
         elif time < timedelta(hours=1):
@@ -72,16 +75,17 @@ class Lecture(models.Model):
         else:
             return 'false'
 
+
 class Reservation(models.Model):
     # 룸 모델
     room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name="reservation_room")
     # 강의 명
     purpose = models.TextField(max_length=100, default='')
-    #예약 시작 시간
+    # 예약 시작 시간
     start_time = models.TimeField(default='')
-    #예약 종료 시간
+    # 예약 종료 시간
     finish_time = models.TimeField(default='')
-    #층
+    # 층
     floor = models.TextField(max_length=10, default='')
 
     def __str__(self):
--- Building_list/views.py
+++ Building_list/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
-from .models import Reservation,Room,Lecture
+from .models import Reservation, Room, Lecture
 from django.utils import timezone
 from datetime import datetime, timedelta, date
+
 
 def building_list(request):
     return render(request, "Building_list/building_list.html")
 
+
 def option_check(request):
     return render(request, "Building_list/option_check.html")
+
 
 def room_list(request):
     if request.method == 'POST':
@@ -21,7 +24,8 @@ def room_list(request):
 
         for i in range(0, rooms.__len__()):
             temp = rooms[i]
-            if ((temp.day_of_the_week == day_of_the_week) and (temp.floor == floors)) or ((temp.day_of_the_week == day_of_the_week) and ('all' == floors)):
+            if ((temp.day_of_the_week == day_of_the_week) and (temp.floor == floors)) or (
+                    (temp.day_of_the_week == day_of_the_week) and ('all' == floors)):
                 room_dict[temp] = i
                 flag = 'true'
                 for j in range(0, lectures.__len__()):
@@ -33,13 +37,15 @@ def room_list(request):
                             temp.save()
                             flag = 'false'
                         else:
-                            time = datetime.combine(date.min, temp.available_time) - datetime.combine(date.min, temp2.start_time)
+                            time = datetime.combine(date.min, temp.available_time) - datetime.combine(date.min,
+                                                                                                      temp2.start_time)
                             if time > timedelta(minutes=1):
                                 temp.available_time = temp2.start_time
                                 temp.name = temp2.name
                                 temp.save()
 
         return render(request, "Building_list/room_list.html", {"rooms": room_dict})
+
 
 def room_detail(request, room_id):
     # lectures = Lecture.objects.all()
